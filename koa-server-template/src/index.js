fb6a689156028a76457c02a740e62167d1228c04
churjan <churjan@163.com>
2020-04-16 16:26:32 +0800

    modify

--- koa-server-template/src/index.js
+++ koa-server-template/src/index.js
@@ -3,6 +3,27 @@ const app = new koa()
 const Router = require('koa-router')
 const router = new Router()
 
+router.post('/mock/queryAll', (ctx, next) => {
+	ctx.body = {
+		status: 200,
+		message: '操作成功',
+		data: [
+			{
+				label: '数据1',
+			},
+			{
+				label: '数据2',
+			},
+			{
+				label: '数据3',
+			},
+			{
+				label: '数据4',
+			},
+		],
+		total: 7,
+	}
+})
 router.post('/mock/pageList', (ctx, next) => {
 	ctx.body = {
 		status: 200,
