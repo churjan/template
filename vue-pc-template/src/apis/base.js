f093e3fe818602cb7fef3c2615b4271bbe66421a
churjan <churjan@163.com>
2020-11-03 15:55:12 +0800

    feat:update

--- vue-pc-template/src/apis/base.js
+++ vue-pc-template/src/apis/base.js
@@ -1,14 +1,11 @@
 import axios from 'axios'
 import qs from 'qs'
 import './interceptor'
-
 export function post(url = '', data = {}, config = {}) {
   data = qs.stringify(data)
   return new Promise((resolve, reject) => {
     axios
-      .post(url, data, {
-        ...config,
-      })
+      .post(url, data, config)
       .then(res => {
         resolve(res)
       })
@@ -18,12 +15,11 @@ export function post(url = '', data = {}, config = {}) {
   })
 }
 export function postJson(url = '', data = {}, config = {}) {
+  console.log(config)
   data = qs.parse(data)
   return new Promise((resolve, reject) => {
     axios
-      .post(url, data, {
-        ...config,
-      })
+      .post(url, data, config)
       .then(res => {
         resolve(res)
       })
@@ -35,7 +31,26 @@ export function postJson(url = '', data = {}, config = {}) {
 export function get(url = '', config = {}) {
   return new Promise((resolve, reject) => {
     axios
-      .get(url, {
+      .get(url, config)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
+}
+export function file(url = '', data = {}, config = {}) {
+  const form = new FormData()
+  Object.keys(data).forEach(key => {
+    form.append(key, data[key])
+  })
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, form, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
         ...config,
       })
       .then(res => {
--- vue-pc-template/src/apis/interceptor.js
+++ vue-pc-template/src/apis/interceptor.js
@@ -5,7 +5,7 @@ axios.interceptors.response.use(
     console.log(response)
     const { status = 0, message = '' } = response.data
     //默认status不为200时，全局会弹toast错误提示
-    const { isShowToast = true } = response.config
+    const { isShowToast = true } = response.config.headers
     if (!isShowToast) {
       return response.data
     }
