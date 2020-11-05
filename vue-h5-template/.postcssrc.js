fc9fefef2813bc4f45c87123d6d987c887d8fe70
churjan <churjan@163.com>
2020-11-05 14:02:06 +0800

    fix:更改代码格式

--- vue-h5-template/.postcssrc.js
+++ vue-h5-template/.postcssrc.js
@@ -2,17 +2,11 @@
 module.exports = {
 	plugins: {
 		autoprefixer: {
-			overrideBrowserslist: [
-				"Android 4.1",
-				"iOS 7.1",
-				"Chrome > 31",
-				"ff > 31",
-				"ie >= 8"
-			]
+			overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8'],
 		},
-		"postcss-pxtorem": {
+		'postcss-pxtorem': {
 			rootValue: 37.5, // vant-UI的官方根字体大小是37.5
-			propList: ["*"]
-		}
-	}
-};
+			propList: ['*'],
+		},
+	},
+}
