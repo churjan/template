96cf4d20a36d79a15f78c691bcf818b90e8d282e
churjan <churjan@163.com>
2020-11-27 13:58:58 +0800

    Revert "modify"
    This reverts commit 6ef6c13ccc36ebff9a6c21918a6409367aff5b63.

--- vue-pc-template/src/views/dashboard/index.vue
+++ vue-pc-template/src/views/dashboard/index.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="box">
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam temporibus iure similique illum quis, debitis
-      sint id laboriosam modi assumenda quisquam quae eos labore maiores aspernatur, consequatur ea? Ipsum, consectetur.
-    </h1>
+  <div>
+    Dashboard
   </div>
 </template>
 
 <script>
-export default {
-  mounted() {
-    setTimeout(() => {
-      const boxEl = document.querySelector('.box')
-      console.log(boxEl.scrollHeight)
-    }, 3000)
-  },
-}
+export default {}
 </script>
 
-<style lang="scss" scoped>
-.box {
-  width: 400px;
-  height: 400px;
-  border: 10px solid red;
-  overflow-x: scroll;
-}
-</style>
+<style lang="scss" scoped></style>
