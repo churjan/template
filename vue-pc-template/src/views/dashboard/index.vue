6ef6c13ccc36ebff9a6c21918a6409367aff5b63
churjan <churjan@163.com>
2020-11-27 13:23:15 +0800

    modify

--- vue-pc-template/src/views/dashboard/index.vue
+++ vue-pc-template/src/views/dashboard/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    Dashboard
+  <div class="box">
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam temporibus iure similique illum quis, debitis
+      sint id laboriosam modi assumenda quisquam quae eos labore maiores aspernatur, consequatur ea? Ipsum, consectetur.
+    </h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    setTimeout(() => {
+      const boxEl = document.querySelector('.box')
+      console.log(boxEl.scrollHeight)
+    }, 3000)
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  width: 400px;
+  height: 400px;
+  border: 10px solid red;
+  overflow-x: scroll;
+}
+</style>
