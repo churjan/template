b87ecaf1d3678af282dddf7e8c3067cd20645ece
Churjan <churjan@163.com>
2021-04-07 17:08:18 +0800

    新增demo

---
+++ uniapp-template/demos/contact/contact.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<cell @startmessage='startmessage' @completemessage="completemessage" :styleType="1" :paddingStyle="20" plugid='275866f1974f4392d93f6eef269f8c40'  />
+		<cell @startmessage='startmessage' @completemessage="completemessage" :styleType="1" :paddingStyle="20" plugid='74537c02e713337d990f198d7cb6a365'  />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			startmessage(e){
+				console.log(e)
+			},
+			completemessage(e){
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
