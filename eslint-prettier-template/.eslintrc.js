8d0c421763b726e917f4efa834b994cefb1b9f4a
Churjan <churjan@163.com>
2021-04-23 13:04:25 +0800

    优化eslint-prettier代码

--- eslint-prettier-template/.eslintrc.js
+++ eslint-prettier-template/.eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-	parser: 'babel-eslint',
-	root: true,
-	env: {
-		browser: true,
-		node: true,
-		es6: true,
-		commonjs: true,
-	},
-	extends: ['plugin:prettier/recommended'],
-	plugins: ['html'],
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{
-				semi: false,
-				singleQuote: true,
-				trailingComma: 'es5',
-				printWidth: 120,
-			},
-		],
-	},
-}
+    parser: "babel-eslint",
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        commonjs: true,
+    },
+    extends: ["plugin:prettier/recommended"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                semi: false,
+                singleQuote: true,
+                trailingComma: "es5",
+                printWidth: 120,
+            },
+        ],
+    },
+};
