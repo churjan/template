844560dfb04e8d0d463afb3ba4f85e0b0767d459
Churjan <churjan@163.com>
2021-04-27 18:47:04 +0800

    删除没有的项目

--- vue2-h5-template/babel.config.js
+++
@@ -1,8 +0,0 @@
-const prodPlugins = []
-if (process.env.NODE_ENV === 'production') {
-    prodPlugins.push('transform-remove-console')
-}
-module.exports = {
-    presets: ['@vue/cli-plugin-babel/preset'],
-    ...prodPlugins,
-}
