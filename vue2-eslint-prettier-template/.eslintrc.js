96c6dbe8ae0fa2c459084334c4986aa4c94d8953
Churjan <churjan@163.com>
2021-10-13 16:11:53 +0800

    modify

--- vue2-eslint-prettier-template/.eslintrc.js
+++ vue2-eslint-prettier-template/.eslintrc.js
@@ -3,12 +3,21 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+      },
+    ],
   },
-};
+}
--- vue2-eslint-prettier-template/src/store/index.js
+++ vue2-eslint-prettier-template/src/store/index.js
@@ -1,11 +1,13 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    publicPath: process.env.BASE_URL,
+  },
   mutations: {},
   actions: {},
   modules: {},
-});
+})
