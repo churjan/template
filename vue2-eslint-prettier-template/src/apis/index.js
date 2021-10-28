e42fbed7b03afe1a6cf618568bc8c1f91937d8cf
Churjan <churjan@163.com>
2021-10-28 20:41:08 +0800

    modify

--- vue2-eslint-prettier-template/src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-const context = require.context('/', false, /\.api\.js$/)
-const apis = {}
-context.keys().forEach((name) => {
-  const key = name.replace(/^\.\//, '').replace(/\.api\.js$/, '')
-  apis[key] = context(name)
-})
-
-Vue.prototype.$apis = apis
