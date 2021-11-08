1a31fba0e6a042c3c2aeb31cf63a2d57d07804ab
Churjan <churjan@163.com>
2021-11-08 18:57:40 +0800

    Update App.vue

--- vue2-eslint-prettier-template/src/App.vue
+++ vue2-eslint-prettier-template/src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <button
-        className="prettier-class dfsd sdfdsfds sfdsf"
-        id="prettier-id"
-        onClick="{this.handleClick}"
-      >
-        Click Here
-      </button>
-    </div>
     <router-view />
   </div>
 </template>
+<style lang="scss">
+@import '@/scss';
+</style>
