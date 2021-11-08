7f2837add0fb9cd944c68280ba66540bbf64caa4
Churjan <churjan@163.com>
2021-11-08 19:57:26 +0800

    Update index.js

--- vue2-eslint-prettier-template/src/store/index.js
+++ vue2-eslint-prettier-template/src/store/index.js
@@ -4,9 +4,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    publicPath: process.env.BASE_URL,
-  },
+  state: {},
   mutations: {},
   actions: {},
   modules: {},
