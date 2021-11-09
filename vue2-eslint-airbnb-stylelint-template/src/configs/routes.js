8bd52056edea0300df8efb4fbe3d9b93097a0a13
Churjan <churjan@163.com>
2021-11-09 17:20:41 +0800

    delete

--- vue2-eslint-airbnb-stylelint-template/src/configs/routes.js
+++
@@ -1,6 +0,0 @@
-export default [
-  {
-    path: '/',
-    component: () => import('@/views/index/index.vue'),
-  },
-];
--- vue2-eslint-airbnb-stylelint-template/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  publicPath: process.env.BASE_URL,
-  outputDir: process.env.VUE_APP_OUT_PUT_DIR,
-};
