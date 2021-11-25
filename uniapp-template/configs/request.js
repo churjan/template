e3395bd7dd1526e9dcbbf80972cc6bd7f090d5e8
Churjan <churjan@163.com>
2021-11-25 21:09:21 +0800

    Update request.js

--- uniapp-template/configs/request.js
+++ uniapp-template/configs/request.js
@@ -24,13 +24,14 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use(
   (response) => {
     const { status, message } = response.data;
-    const { isShowToast = true } = response.config;
+    const { isShowToast = true } = response.config.custom;
     if (!isShowToast) {
       return response.data;
     }
     if (status !== 200) {
       uni.showToast({
         title: message,
+        icon: 'none',
       });
       return Promise.reject(message);
     }
@@ -40,6 +41,7 @@ http.interceptors.response.use(
     console.log(error);
     uni.showToast({
       title: '接口报错',
+      icon: 'none',
     });
     return Promise.reject(error);
   }
